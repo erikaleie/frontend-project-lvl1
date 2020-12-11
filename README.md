2cf179a8c88c4a3d8b6cc2192b2032ee72dfb15d
erikaleie <bogachevanadia@yandex.ru>
2020-12-11 13:51:04 +0100

    Add brain-calc

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/erikaleie/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/erikaleie/frontend-project-lvl1/actions)
 <a href="https://codeclimate.com/github/erikaleie/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/a17e00d52befec30ec37/maintainability" /></a>
-<img src="https://github.com/erikaleie/frontend-project-lvl1/workflows/linter/badge.svg" /></a> 
-<a href="https://asciinema.org/a/3MTQdsyn0GcE6oUv0t0YSy8e3">asciinema</a>
+<img src="https://github.com/erikaleie/frontend-project-lvl1/workflows/linter/badge.svg" /></a>
+</br> 
+<a href="https://asciinema.org/a/3MTQdsyn0GcE6oUv0t0YSy8e3">Brain-even</a>
+</br> 
+<a href="https://asciinema.org/a/3oHZfJP2xY1anmofLZuYXgEjN">Brain-calc</a>
+
---
+++ bin/brain-calc.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+
+console.log('What is the result of the expression?');
+
+const n = 3;
+let check = false;
+let i = 0;
+
+while (check === false) {
+  const a = Math.floor(Math.random() * 101);
+  const b = Math.floor(Math.random() * 10);
+
+  const o = ['-', '+', '*'];
+  let oper = o[Math.floor(Math.random() * 3)];
+
+  console.log(`${a} ${oper} ${b}`);
+  const answer = readlineSync.question('Your answer: ');
+
+  let res; 
+
+  switch (oper) {
+    case '-':
+      res = a - b;
+      //Здесь выполняются инструкции, если результат выражения равен value1
+      break;
+    case '+':
+      res = a + b;
+      //Инструкции, соответствующие value2
+      break;
+    case '*':
+      res = a * b;
+      //Инструкции, соответствующие значению valueN
+      //statementsN
+      break;
+  } 
+
+  if (res === Number(answer)) {
+    i += 1;
+    console.log('Correct!');
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was ${res}.\nLet's try again, ${userName}`);
+    break;
+  }
+
+  if (i === n) {
+    check = true;
+    console.log(`Congratulations, ${userName}`);
+  }
+}
